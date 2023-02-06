3c829207912cde2a4e58c5cf7702ed87dd25b3fd
csmcorporatama@gmail.com <indorent10>
2023-02-06 10:51:07 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ParameterizedQuery.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
